5b307c238480b435be2adbec42a9211c25cf3910
hourglass12 <piledriverwaltz1112@gmail.com>
2024-03-20 17:33:24 +0900

    initial commit

---
+++ face_on_display/face_on_display_app.py
@@ -0,0 +1,90 @@
+from depth_estimation import DepthEstimation
+import tkinter as tk
+from util.tk_color import rgb_code2rgb
+import cv2
+import numpy as np
+from PIL import Image, ImageTk
+import time
+
+time_measure = False
+
+class FaceOnDisplayApp:
+    BG_COLOR = "#0000FF"
+
+    def __init__(self):
+        self.root = tk.Tk()
+        # ウィンドウを全画面に設定
+        self.root.attributes('-fullscreen', True)
+        self.root.config(bg = self.BG_COLOR)
+        # ウィンドウの背景を透明に設定
+        #self.root.attributes('-alpha', 1.0)  # 透明度を1.0に設定（不透明）
+        self.root.wm_overrideredirect(True)
+        self.root.wm_attributes("-transparentcolor", self.BG_COLOR)
+        # 透明な背景を持つキャンバスを作成
+        self.canvas = tk.Canvas(self.root, bg=self.BG_COLOR, highlightthickness=0)
+        self.canvas.pack(fill='both', expand=True)
+        # ウィンドウを閉じるためのキーバインディングを追加
+        self.root.bind('<Escape>', self.close_window)
+
+        # カメラのキャプチャを開始
+        self.cap = cv2.VideoCapture(0)
+
+        self.update_frame()  # フレームを更新する関数を呼び出し
+
+        self.root.mainloop()
+
+    def close_window(self, event):
+        self.root.destroy()
+    
+    def update_frame(self):
+        ret, frame = self.cap.read()
+        if ret:
+            if time_measure:
+                start = time.perf_counter()
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            # ウィンドウのサイズを取得
+            window_width = self.root.winfo_screenwidth()
+            window_height = self.root.winfo_screenheight()
+
+            # 画像のサイズを取得
+            image_height, image_width, _ = frame.shape
+
+            # ウィンドウのアスペクト比を計算
+            window_aspect_ratio = window_width / window_height
+
+            # 画像のアスペクト比を計算
+            image_aspect_ratio = image_width / image_height
+
+            if window_aspect_ratio > image_aspect_ratio:
+                # ウィンドウのアスペクト比が画像のアスペクト比より大きい場合、ウィンドウの高さを画像の高さに合わせる
+                display_height = window_height
+                display_width = int(window_height * image_aspect_ratio)
+            else:
+                # 画像のアスペクト比がウィンドウのアスペクト比より大きい場合、ウィンドウの幅を画像の幅に合わせる
+                display_width = window_width
+                display_height = int(window_width / image_aspect_ratio)
+
+            # 画像をリサイズ
+            frame = cv2.resize(frame, (display_width, display_height))
+
+            # 深度推定画像に変換
+            depth_estimated_mask = DepthEstimation().get_estimate_depth_mask(frame, 190)
+            frame[np.logical_not(depth_estimated_mask)] = rgb_code2rgb(self.BG_COLOR)
+            img = Image.fromarray(frame)
+            imgtk = ImageTk.PhotoImage(image=img)
+
+            # 以前の画像を削除
+            self.canvas.delete("all")
+            # キャンバス上に画像を描画
+            self.canvas.create_image((window_width - display_width) // 2, (window_height - display_height) // 2, anchor="nw", image=imgtk)
+            
+            # 画像が参照され続けるようにラベルオブジェクトを保持
+            self.canvas.imgtk = imgtk
+            if time_measure:
+                print(time.perf_counter() - start)
+
+        self.canvas.after(10, self.update_frame)  # 10ミリ秒ごとにフレームを更新
+
+if __name__ == '__main__':
+    FaceOnDisplayApp()
---
+++ face_on_display/util/tk_color.py
@@ -0,0 +1,37 @@
+import tkinter as tk
+import time
+
+def rgb_rgb2code(R, G, B):
+    rgb_code = '#'
+    for c in [R, G, B]:
+        rgb_code += f"{c:0=2x}"
+    return rgb_code
+
+def rgb_code2rgb(rgb_code):
+    r, g, b = [rgb_code[1+2*i:3+2*i] for i in range(3)][:]
+    rgb = []
+    for c in [r, g, b]:
+        rgb.append(int(c, 16))
+    return rgb
+
+# referred to https://www.ishikawasekkei.com/index.php/2020/03/30/python-tkinter-predefined-color/
+def rgb_name2code(color_name):
+    root = tk.Tk()
+    rgb = root.winfo_rgb(color_name)
+    rgb_code  = "#%02X%02X%02X"%(rgb[0]//256, rgb[1]//256, rgb[2]//256)
+    root.destroy()
+    return rgb_code
+
+# too weight! takes 0.1s
+def rgb_name2rgb(color_name):
+    start = time.perf_counter()
+    root = tk.Tk()
+    rgb = root.winfo_rgb(color_name)
+    root.destroy()
+    print(time.perf_counter() - start)
+    return list(map(lambda c:c//256, rgb))
+
+if __name__ == '__main__':
+    #print(rgb_name2code("blue"))
+    #print(rgb_name2rgb("blue"))
+    print(rgb_code2rgb("#0000FF"))
