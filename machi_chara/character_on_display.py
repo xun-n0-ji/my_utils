8c938818b0ac6aef5237f9804f2bcd2d3bec1c0d
hourglass12 <piledriverwaltz1112@gmail.com>
2024-03-20 19:37:04 +0900

    moves in any direction

--- machi_chara/character_on_display.py
+++ machi_chara/character_on_display.py
@@ -1,6 +1,8 @@
 import tkinter as tk
-from gif_edit.mask_GB import get_mask
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 from util.tk_color import rgb_code2rgb
+from util.mask_greenback import get_mask
 import cv2
 import numpy as np
 from PIL import Image, ImageTk
@@ -11,7 +13,7 @@ time_measure = False
 class CharacterOnDisplayApp:
     BG_COLOR = "#0000FF"
 
-    def __init__(self, video_path, r = 1.0):
+    def __init__(self, video_path, r = 1.0, monitor_index = 0):
         self.root = tk.Tk()
         # ウィンドウを全画面に設定
         self.root.attributes("-topmost", True)
@@ -24,13 +26,6 @@ class CharacterOnDisplayApp:
         self.canvas = tk.Canvas(self.root, bg=self.BG_COLOR, highlightthickness=0)
         self.canvas.bind('<Button-1>', self.close_window)
         self.canvas.pack(fill='both', expand=True)
-        
-        # window direction (1:right, -1:left)
-        self.direction_x = 1
-        self.direction_y = 1
-        #self.move_angular = np.random.randint(270, 360)*np.pi/180
-        self.moved_distance_x = 0
-        self.moved_distance_y = 0
 
         # ウィンドウを閉じるためのキーバインディングを追加
         self.root.bind('<Escape>', self.close_window)
@@ -42,8 +37,14 @@ class CharacterOnDisplayApp:
         self.window_width = int(self.video.get(cv2.CAP_PROP_FRAME_WIDTH) * r)
         self.window_height = int(self.video.get(cv2.CAP_PROP_FRAME_HEIGHT) * r)
 
-        self.select_monitor(1)
+        self.monitor_index = monitor_index
+        self.select_monitor(self.monitor_index)
         #self.root.geometry(f"{self.window_width}x{self.window_height}")
+
+        # window direction (1:right, -1:left)
+        self.move_angular = np.random.randint(0, 360)*np.pi/360
+        self.direction_x = 1 if np.cos(self.move_angular) >= 0 else -1
+        self.direction_y = 1 if np.sin(self.move_angular) >= 0 else -1
         
         # 動画からフレーム数を取得する
         self.total_frames = int(self.video.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -65,30 +66,33 @@ class CharacterOnDisplayApp:
         if monitor_index < 0:
             return  # インデックスが負の場合はデフォルトのモニターに表示
 
-        # モニターの解像度を取得
-        monitor_width = self.screen_width
-        monitor_height = self.screen_height
-
-        # ウィンドウの位置を計算
-        self.x = -monitor_width * monitor_index # initial
-        self.y = 0  # initial ウィンドウを上端に表示する場合
+        # ウィンドウの位置を計算（端で起動すると角度計算が面倒なので上下左右1ピクセルずつマージンを設けている）
+        self.x = -self.screen_width * monitor_index + np.random.randint(1, self.screen_width - self.window_width - 1) # initial
+        self.y = np.random.randint(1, self.screen_height - self.window_height - 1) # initial
 
         self.root.geometry(f"{self.window_width}x{self.window_height}+{self.x}+{self.y}")
 
     def move_window(self, step):
-        width, height = self.root.winfo_width(), self.root.winfo_height()
         # ウィンドウの移動方向を考慮して新しい位置を計算
-        self.x += self.direction_x * step
-        self.y += self.direction_y * step
-        self.moved_distance_x += step
-        self.moved_distance_y += step
+        self.x += int(step*np.cos(self.move_angular))
+        self.y += int(step*np.sin(self.move_angular))
         # 右端または左端に到達した場合は方向を反転
-        if self.moved_distance_x >= self.screen_width - self.window_width:
-            self.direction_x *= -1
-            self.moved_distance_x = 0
-        if self.moved_distance_y >= self.screen_height - self.window_height:
-            self.direction_y *= -1
-            self.moved_distance_y = 0
+        if self.x + self.screen_width * self.monitor_index >= self.screen_width - self.window_width:
+            self.move_angular = np.random.randint(0, 180)*np.pi/360 + np.pi/2
+            self.direction_x = 1 if np.cos(self.move_angular) >= 0 else -1
+            self.direction_y = 1 if np.sin(self.move_angular) >= 0 else -1
+        elif self.x + self.screen_width * self.monitor_index <= 0:
+            self.move_angular = np.random.randint(0, 180)*np.pi/360 - np.pi/2
+            self.direction_x = 1 if np.cos(self.move_angular) >= 0 else -1
+            self.direction_y = 1 if np.sin(self.move_angular) >= 0 else -1 
+        if self.y >= self.screen_height - self.window_height:
+            self.move_angular = np.random.randint(0, 180)*np.pi/360 + np.pi
+            self.direction_x = 1 if np.cos(self.move_angular) >= 0 else -1
+            self.direction_y = 1 if np.sin(self.move_angular) >= 0 else -1
+        elif self.y <= 0:
+            self.move_angular = np.random.randint(0, 180)*np.pi/360
+            self.direction_x = 1 if np.cos(self.move_angular) >= 0 else -1
+            self.direction_y = 1 if np.sin(self.move_angular) >= 0 else -1
         self.root.geometry(f"{self.window_width}x{self.window_height}+{self.x}+{self.y}")
     
     def update_frame(self):
@@ -124,4 +128,5 @@ class CharacterOnDisplayApp:
         self.move_window(step = 5)
 
 if __name__ == '__main__':
-    CharacterOnDisplayApp("image\ms_minutes_walking_GB - Made with Clipchamp.mp4", r=0.5)
+    image_dirpath = fr"{os.path.dirname(os.path.abspath(__file__))}/image"
+    CharacterOnDisplayApp(fr"{image_dirpath}\ms_minutes_walking_GB.mp4", r=0.5, monitor_index=1)
